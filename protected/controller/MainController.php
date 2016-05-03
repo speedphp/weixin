46ba9d578ddfa6a849b1e02457a8521f688d77bb
jake <webmaster@speedphp.com>
2016-05-03 15:04:17 +0800

    book31应该在book前面

--- protected/controller/MainController.php
+++ protected/controller/MainController.php
@@ -30,15 +30,13 @@ class MainController extends BaseController {
 	}
 
 	var $commands = array(
+		"book31" => array("sp31", "book", "3.1版手册"),
 		"new"  => array("spNew", "intro", "新版介绍"),
 		"book" => array("spNew", "book", "新版手册"),
 		"code" => array("spNew", "code", "新版框架源码"),
 		"log"  => array("spNew", "changelog", "新版更新日志"),
-
 		"intro"  => array("sp31", "intro", "3.1版介绍"),
-		"book31" => array("sp31", "book", "3.1版手册"),
 		"smarty" => array("sp31", "smarty", "Smarty手册"),
-
 		"case"  => array("web", "case", "案例展示"),
 		"git"   => array("web", "git", "Github库"),
 		"weibo" => array("web", "weibo", "微博"),
